2206ae9ccf9d35c1f36f285ea2b14ba48577a3a4
Tim Mees <tim.mees83@gmail.com>
2023-01-29 19:55:41 +0100

    Add run check to firstboot script

--- firstboot/firstboot.sh
+++ firstboot/firstboot.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
 firstboot_scripts=$(ls /usr/libexec/fedora-autofirstboot/scripts.d/*.sh)
-	
-for firstboot_script in ${firstboot_scripts[@]}
-do
-   . "${firstboot_script}"
+run_dir=/var/cache/firstboot_run
+
+mkdir -p "${run_dir}"
+for firstboot_script in ${firstboot_scripts[@]}; do
+   run_file="${run_dir}/$(sha1sum ${firstboot_script})_${firstboot_script}"
+   if [ ! -e ${run_file} ]; then
+      . "${firstboot_script}" && touch $run_file
+   fi
 done

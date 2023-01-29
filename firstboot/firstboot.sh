45d627bf7ff3dde16e786f850bb06105739e775d
Tim Mees <tim.mees83@gmail.com>
2023-01-29 20:17:53 +0100

    Fix firstboot script

--- firstboot/firstboot.sh
+++ firstboot/firstboot.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-firstboot_scripts=$(ls /usr/libexec/fedora-autofirstboot/scripts.d/*.sh)
+firstboot_scripts=$(ls /etc/firstboot/scripts.d/*.sh)
 run_dir=/var/cache/firstboot_run
 
 mkdir -p "${run_dir}"
 for firstboot_script in ${firstboot_scripts[@]}; do
-   run_file="${run_dir}/$(sha1sum ${firstboot_script})_${firstboot_script}"
-   if [ ! -e ${run_file} ]; then
+   run_file="${run_dir}/$(sha1sum ${firstboot_script} | cut -d ' ' -f1)_$(basename ${firstboot_script})"
+   if [ ! -e "${run_file}" ]; then
       . "${firstboot_script}" && touch $run_file
    fi
 done

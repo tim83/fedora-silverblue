944b08dd753bc657acca64db2037cecb789e21fe
Tim Mees <tim.mees83@gmail.com>
2023-04-24 16:46:16 +0200

    Format shells

--- firstboot/scripts.d/graphical.sh
+++ firstboot/scripts.d/graphical.sh
@@ -7,6 +7,7 @@
 # Install flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-modify --enable flathub
+
 # Install flatpaks
 flatpak install --noninteractive flathub \
         org.mozilla.firefox \
